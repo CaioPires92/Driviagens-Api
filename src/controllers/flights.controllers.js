fe83b177db6230bc4488970bf9ecf670e6418e30
Caio Pires <caiogodoi_92@hotmail.com>
2023-09-10 16:57:59 -0300

    created flights controller

--- src/controllers/flights.controllers.js
+++ src/controllers/flights.controllers.js
@@ -1,5 +1,39 @@
+import dayjs from 'dayjs'
+import { db } from '../database/db.connection.js'
+
 export async function createFlights(req, res) {
-  res.send('flights')
+  const { origin, destination, date } = req.body
+
+  try {
+    const cityOrigin = await db.query(`SELECT * FROM cities WHERE id=$1`, [
+      origin
+    ])
+
+    const citydestination = await db.query(`SELECT * FROM cities WHERE id=$1`, [
+      destination
+    ])
+
+    if (cityOrigin.rowCount === 0 || citydestination.rowCount === 0) {
+      return res
+        .status(404)
+        .send({ message: 'Cidade de origem ou destino não existe' })
+    }
+
+    if (cityOrigin.rows[0].id === citydestination.rows[0].id) {
+      return res.status(409).send({
+        message: 'a cidade de origem e destino não podem ser as mesmas'
+      })
+    }
+
+    await db.query(
+      `INSERT INTO flights (origin, destination, data) VALUES ($1, $2, $3);`,
+      [origin, destination, date]
+    )
+
+    return res.status(201).json({ message: 'Voo criado com sucesso' })
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
 }
 
 export async function getFlights(req, res) {
--- src/schemas/flights.schema.js
+++ src/schemas/flights.schema.js
@@ -1,9 +1,24 @@
 import Joi from 'joi'
+import dayjs from 'dayjs'
+
+// Função de validação personalizada para datas no formato "dd-mm-yyyy"
+const customDateValidator = (value, helpers) => {
+  const date = dayjs(value, 'DD-MM-YYYY', true) // Parse da data no formato "dd-mm-yyyy"
+  const currentDate = dayjs()
+
+  if (!date.isValid()) {
+    return helpers.message('Data no formato "dd-mm-yyyy" inválida')
+  }
+
+  if (date.isBefore(currentDate, 'day')) {
+    return helpers.message('A data do voo deve ser maior do que a data atual')
+  }
+
+  return value // Data válida
+}
 
 export const flightSchema = Joi.object({
   origin: Joi.number().integer().min(1).required(),
   destination: Joi.number().integer().min(1).required(),
-  date: Joi.string()
-    .regex(/^\d{2}-\d{2}-\d{4}$/)
-    .required()
+  date: Joi.string().custom(customDateValidator, 'custom validation').required()
 })
