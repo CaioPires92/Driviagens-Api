22c6b8a74dcd5f121248b446b1bed54f1c74e992
Caio Pires <caiogodoi_92@hotmail.com>
2023-09-10 22:44:06 -0300

    created getPessengersTravels

--- src/controllers/passengers.controlles.js
+++ src/controllers/passengers.controlles.js
@@ -16,5 +16,36 @@ export async function createPassengers(req, res) {
 }
 
 export async function getPessengersTravels(req, res) {
-  res.send('/passengers/travels')
+  const { name } = req.query
+
+  try {
+    const maxResults = 10
+    if (maxResults < 1) {
+      return res.status(500).json({ message: 'Too many results' })
+    }
+
+    let query = `
+    SELECT CONCAT(passengers.firstname, ' ', passengers.lastname) AS passenger, COUNT(travels.id) AS travels
+    FROM passengers
+    LEFT JOIN travels ON passengers.id = travels.passengerid
+    WHERE 1=1`
+
+    const params = []
+
+    if (name) {
+      query +=
+        " AND CONCAT(passengers.firstname, ' ', passengers.lastname) ILIKE $1"
+      params.push(`%${name}%`)
+    }
+
+    query += " GROUP BY CONCAT(passengers.firstname, ' ', passengers.lastname)"
+    query += ' ORDER BY travels DESC'
+    query += ` LIMIT ${maxResults}`
+
+    const result = await db.query(query, params)
+    res.json(result.rows)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Erro interno do servidor' })
+  }
 }
