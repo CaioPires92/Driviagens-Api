1f47699dccc7952bbd5093c3edbd412cec924a0d
Caio Pires <caiogodoi_92@hotmail.com>
2023-09-10 22:15:23 -0300

    created getFlights

--- src/controllers/flights.controllers.js
+++ src/controllers/flights.controllers.js
@@ -2,7 +2,7 @@ import dayjs from 'dayjs'
 import { db } from '../database/db.connection.js'
 
 export async function createFlights(req, res) {
-  const { origin, destination, date } = req.body
+  const { origin, destination, date } = req.body || req.params
 
   try {
     const cityOrigin = await db.query(`SELECT * FROM cities WHERE id=$1`, [
@@ -37,5 +37,53 @@ export async function createFlights(req, res) {
 }
 
 export async function getFlights(req, res) {
-  res.send('flights')
+  try {
+    const { origin, destination, smallerDate, biggerDate } = req.query
+
+    // Validação de datas usando Joi (lembre-se de definir seu schema de validação)
+    // Se a validação falhar, você pode retornar um erro 422 aqui
+
+    // Montar a consulta SQL com base nos parâmetros
+    let query = `
+      SELECT flights.id, cities_origin.name AS origin, cities_destination.name AS destination, flights.date
+      FROM flights
+      INNER JOIN cities AS cities_origin ON flights.origin = cities_origin.id
+      INNER JOIN cities AS cities_destination ON flights.destination = cities_destination.id
+      WHERE 1=1`
+
+    const params = []
+
+    if (origin) {
+      query += ' AND cities_origin.name = $1'
+      params.push(origin)
+    }
+
+    if (destination) {
+      query += ' AND cities_destination.name = $2'
+      params.push(destination)
+    }
+
+    if (smallerDate && biggerDate) {
+      query += ' AND flights.date BETWEEN $3 AND $4'
+      params.push(smallerDate, biggerDate)
+    }
+
+    // Ordenar por data
+    query += ' ORDER BY flights.date'
+
+    // Executar a consulta no banco de dados
+    const result = await db.query(query, params)
+
+    const flights = result.rows.map(row => ({
+      id: row.id,
+      origin: row.origin,
+      destination: row.destination,
+      date: dayjs(row.date).format('DD-MM-YYYY') // Formatar a data aqui
+    }))
+
+    res.json(flights)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Erro interno do servidor' })
+  }
 }
--- src/controllers/travels.controllers.js
+++ src/controllers/travels.controllers.js
@@ -1,3 +1,27 @@
+import { db } from '../database/db.connection.js'
+
 export async function createTravels(req, res) {
-  res.send('travels')
+  const { passengerId, flightId } = req.body
+
+  try {
+    const passenger = await db.query(`SELECT * FROM passengers WHERE id=$1`, [
+      passengerId
+    ])
+    const flight = await db.query(`SELECT * FROM flights WHERE id=$1`, [
+      flightId
+    ])
+
+    if (passenger.rowCount === 0 || flight.rowCount === 0) {
+      return res.status(404).send('passejeiro ou voo não encontrado!')
+    }
+
+    await db.query(
+      `INSERT INTO travels (passengerId, flightId) VALUES ($1, $2)`,
+      [passengerId, flightId]
+    )
+
+    return res.sendStatus(201)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
 }
